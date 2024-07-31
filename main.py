b14f03214e41beac30b55aac50344a42b81463f4
akbay74 <asv1974@list.ru>
2024-07-31 09:15:05 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+print((9**0.5)*5)
+
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3rd program
+a = (1234 % 1000) // 10
+b = (5678 % 1000) // 10
+print(a, b)
+
+# 4th program
+a = int(13.42)
+b = int((42.13 * 100) % 100)
+print(a == b)
